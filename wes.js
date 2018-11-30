46961d45db5a28009dc35b537a5d5ef1f36ce72f
youssefrif14 <45339437+youssefrif14@users.noreply.github.com>
2018-11-30 20:26:05 +0300

    Update wes.js

--- wes.js
+++ wes.js
@@ -7,8 +7,8 @@ console.log(`Logged in as ${client.user.tag}!`);
 
 
 client.on('ready', async() => {
-var server = "512556758833823764";//ايدي السيرفر
-var channel = "512556850558795777";//ايدي الروم
+var server = "517767098043203616";//ايدي السيرفر
+var channel = "518107578526466060";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا ')
 	},100);
@@ -16,10 +16,10 @@ var channel = "512556850558795777";//ايدي الروم
 
 client.on('message', message => {
     let prefix = 'البريفكس';
-   if(message.content.startsWith('say')) {
+   if(message.content.startsWith('123say')) {
        let args = message.content.split(' ').slice(1).join(' ');
        message.channel.send(args)
    } 
 });
 
-client.login("توكن");
+client.login("NTE4MTE1MTkxODA0Nzg4NzM2.DuMD4A.RN_z4k--IFB2VPVZnlF_C8p6lLA");
